86eabfae01ef4c4608b2095c69f9a2f25f2518bc
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 00:45:36 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {:patriarch = {},
       :matriarch = {},
       :hero = {},
-      :hero_friends = {[]}
+      :hero_friends = []
       },
     :capulet => {:patriarch => {},
      :matriarch => {},
